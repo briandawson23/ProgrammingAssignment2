8f826c02744288bb9b03a31a0e6d9e56d0a925a1
briandawson23 <bdawson@blazent.com>
2015-04-26 01:34:28 -0400

    Dawson - Initial checkin Date - 4/26/2015

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,75 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programmer: Brian Dawson
+## Date: 4/24/2015
+## Description:
+## These two functions will cache the inverse of a matrix by
+## first, creating a matrix that can cache its inverse and
+## secondly, returning the inverse of the matrix.  If the inverse
+## has already been calculated, it will be returned in cache rather
+## than calculating the inverse each time, hopefully providing 
+## significant performance improvements.
 
-## Write a short comment describing this function
+
+## Programmer: Brian Dawson
+## Date: 4/24/2015
+## Function: makeCacheMatrix
+## Description: Returns a matrix that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+           inv <- NULL
+            set <- function(y) {
+                    x <<- y
+                    inv <<- NULL
+            }
+            get <- function() x
+            
+			setinv <- function(inverse) {
+				inv <<- inverse
+			}
+            
+			getinv <- function() {
+				inv
+			}
+			
+            list(set = set, get = get,setinv = setinv,getinv = getinv)
+			
 }
 
-
-## Write a short comment describing this function
+## Programmer: Brian Dawson
+## Date: 4/24/2015
+## Function: cacheSolve
+## Description: Return the inverse of a matrix by cache if already present.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    
+     cacheInv <- x$getinv()
+  
+      if(!is.null(cacheInv)) {
+          message("Getting cached data")
+          return(cacheInv)
+      }
+      
+      data <- x$get()
+      
+      m <- solve(data, ...)
+      
+      x$setinv(m)
+      
+      m
+}
+
+
+testProgram = function(testMat){
+      
+        temp = makeCacheMatrix(testMat)
+        
+        start = Sys.time()
+        cacheSolve(temp)
+        duration = Sys.time() - start
+        print(duration)
+
+        start = Sys.time()
+        cacheSolve(temp)
+        duration = Sys.time() - start
+        print(duration)
 }
